c7998744d7a8679415fffd80fc96fcf4b1479022
Dmytro Krempovskyy <krempovskyydima@gmail.com>
2024-09-06 09:48:16 +0300

    Update App.java
    made the code more clear and moved variables to the top

--- src/App.java
+++ src/App.java
@@ -1,31 +1,34 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Printer app");
+        String creator = "Dima";
         Integer number1 = 10;
         Integer number2 = 3;
-        String creator = "Dima";
+        Integer multiplication = number1 * number2;
+        Integer addition = number1 + number2;
+        double division = number1 / number2;
+        Integer substraction = number1 - number2;
+
+        
+        System.out.println("Printer app");
         System.out.print("This app is made by: ");
         System.out.println(creator);
 
-        Integer addition = number1 + number2;
         System.out.println("The value of number1 is " + number1);
         System.out.println("The value of number2 is " + number2);
-        
-        Integer multiplication = number1 * number2;
+
         System.out.print(number1 + " * " + number2);
         System.out.println(" = " + multiplication);
 
         System.out.print(number1 + " + " + number2);
         System.out.println(" = " + addition);
 
-        double division = number1 / number2;
         System.out.print(number1 + " / " + number2);
         System.out.println(" = " + division);
 
-        Integer substraction = number1 - number2;
         System.out.print(number1 + " - " + number2);
         System.out.println(" = " + substraction);
+
 
     }
 }
